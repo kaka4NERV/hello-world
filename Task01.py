2f9c13d1d6f0ede06cb79871fb687083230e8b18
kaka4NERV <ampedee@163.com>
2018-08-20 17:19:41 +0800

    重构代码，完成更新计划1

--- Task01.py
+++ Task01.py
@@ -1,5 +1,6 @@
 from sys import exit
 from textwrap import dedent
+from time import sleep
 
 
 class Scene(object):
@@ -8,7 +9,6 @@ class Scene(object):
         print("Nothing there")
         exit(1)
 
-    first_scene = False
     find_cave = False
     find_meat = False
     find_sword = False
@@ -38,7 +38,7 @@ class Death(Scene):
         print(dedent("""
             你进入了洞穴深处，
             发现了小女孩的衣服及尸体残肢，
-            以及洞穴更深处一双幽绿的眼，
+            以及洞穴更深处一双幽绿的眼睛，
             一头凶恶的狼人从黑暗中向你扑来，
             没有熬制狼人药水，你的银剑只能擦伤狼人坚硬的皮毛，
             这场战斗你毫无胜算，你死了。
@@ -83,46 +83,28 @@ class BloodScent(Scene):
             """))
 
     def enter(self):
-        if not Scene.first_scene:
-            Scene.first_scene = True
+        if not Scene.find_cave:
             Scene.find_cave = True
-            self.message()
-            choice = input(dedent("""
+
+        self.message()
+        choice = input(dedent("""
                     采取以下行动：
                     1.深入洞穴
                     2.返回河边
                     请输入数字进行选择-->
                     """))
-            if choice == '1':
-                return 'death'
-            elif choice == '2':
-                return 'riverside'
+        if choice == '1':
+            if Scene.find_sword:
+                return 'ending02'
+            elif Scene.find_meat:
+                return 'ending01'
             else:
-                print("请按提示进行选择！")
-                return 'riverside'
+                return 'death'
+        elif choice == '2':
+            return 'riverside'
         else:
-            self.message()
-            if Scene.find_meat:
-                    return 'ending01'
-            elif Scene.find_sword:
-                    return 'ending02'
-            elif Scene.find_cave:
-                    pass
-            else:
-                    return 'riverside'
-            choice = input(dedent("""
-                    采取以下行动：
-                    1.深入洞穴
-                    2.返回河边
-                    请输入数字进行选择-->
-                    """))
-            if choice == '1':
-                return 'death'
-            elif choice == '2':
-                return 'riverside'
-            else:
-                print("你输入了错误的回答！")
-                return 'finished'
+            print("请按提示进行选择！")
+            return 'riverside'
 
 
 class BrokenMeat(Scene):
@@ -133,32 +115,30 @@ class BrokenMeat(Scene):
             你无法分辨腐肉属于哪种动物，
             但直觉告诉你这不属于人类。
             你在天黑前找到了村里的猎人，
-            他告诉你这是狼肉，
-            而且是狼心的碎片。
+            他告诉你这闻起来像是狼肉，
+            而且看起来像是狼的心脏。
             狩魔猎人们都知道，
-            狼心肉可能会引来并激怒一种凶猛的怪物-狼人，
-            这个小女孩凶多吉少了。
+            可以用狼的心脏作为诱饵吸引一种凶猛的怪物-狼人，
+            看来这个小女孩凶多吉少了。
             """))
+        sleep(5)
         print(dedent("""
             你已经获知了这次任务可能会与狼人扯上关系，
             因此你熬制了一瓶狼人药水并涂在银剑上。
             """))
 
     def enter(self):
-        if not Scene.first_scene:
-            Scene.first_scene = True
+        if not Scene.find_meat:
             Scene.find_meat = True
             self.message()
+            print("你回到了河边")
             return 'riverside'
         else:
             self.message()
-            if Scene.find_cave:
-                return 'ending01'
-            elif Scene.find_sword:
+            if Scene.find_sword:
                 return 'ending03'
-            elif Scene.find_meat:
-                return 'riverside'
             else:
+                print("你回到了河边")
                 return 'riverside'
 
 
@@ -168,38 +148,36 @@ class WoodenSword(Scene):
     def message():
         print(dedent("""
             你拿着木剑找到了村长，
-            他认出了这是凯拉的朋友瑞克的。
-            你来到了瑞克家，他看上去惊慌而又害怕。
+            他认出了这是Kara的朋友Rick的。
+            你来到了Rick家，他看上去惊慌而又害怕。
             他对你说，
-            铁匠曾经告诉凯拉河边埋有宝藏，
-            于是昨晚凯拉叫上他带着木剑来到河边。
+            铁匠曾经告诉Kara河边埋有宝藏，
+            于是昨晚Kara叫上他带着木剑来到河边。
             但他们只挖出了一块散发着臭气的烂肉，
             后来他看到树林中有一头直立行走的狼，
             于是害怕的一个人逃走了。
             """))
+        sleep(5)
         print(dedent("""
             你获得了两条关键信息：
-            凯拉遇上了狼人，恐怕已经凶多吉少；
+            Kara遇上了狼人，恐怕已经凶多吉少；
             事情可能与铁匠有关,但你需要更多证据。
             你已经获知了这次任务可能会与狼人扯上关系，
             因此你熬制了一瓶狼人药水并涂在银剑上。
             """))
 
     def enter(self):
-        if not Scene.first_scene:
-            Scene.first_scene = True
+        if not Scene.find_sword:
             Scene.find_sword = True
             self.message()
+            print("你回到了河边")
             return 'riverside'
         else:
             self.message()
-            if Scene.find_cave:
-                return 'ending02'
-            elif Scene.find_meat:
+            if Scene.find_meat and Scene.find_cave is False:
                 return 'ending03'
-            elif Scene.find_sword:
-                return 'riverside'
             else:
+                print("你回到了河边")
                 return 'riverside'
 
 
@@ -214,18 +192,19 @@ class Ending01(Scene):
 
     def enter(self):
         print(dedent("""
-            你进入了森林洞穴深处，
+            你进入了洞穴深处，
             发现了小女孩的衣服及尸体残肢，
             以及洞穴更深处一双幽绿的眼睛，
             一头凶恶的狼人从黑暗中向你扑来，
             你与狼人展开了战斗，
             涂过狼人药剂的银剑对付狼人十分有效，
-            你赢了。
-            你将凯拉的剩余部分用衣服包起，
+            你杀死了狼人。
+            你将Kara的剩余部分用衣服包起，
             愿她能在别处安息。
             """))
+        sleep(5)
         print(dedent("""
-            你将凯拉的遗骸交给村长，
+            你将Kara的遗骸交给村长，
             并带着赏金回到了狩魔猎人城堡。
             任务结束。
             """))
@@ -238,15 +217,15 @@ class Ending02(Scene):
         print(dedent("""
             你进入了洞穴深处，
             发现了小女孩的衣服及尸体残肢，
-            以及洞穴更深处一双幽绿的眼，
+            以及洞穴更深处一双幽绿的眼睛，
             一头凶恶的狼人从黑暗中向你扑来，
             你与狼人展开了战斗，
             涂过狼人药剂的银剑对付狼人十分有效，
             你杀死了狼人。
-            你将凯拉的剩余部分用衣服包起，
+            你将Kara的剩余部分用衣服包起，
             愿她能在别处安息。
             """))
-
+        sleep(5)
         print(dedent("""
             你在狼人尸体上捡到了一件精巧的铁铸饰物，
             这种锻造技巧并不多见，
@@ -259,7 +238,7 @@ class Ending03(Scene):
 
     def enter(self):
         print(dedent("""
-            你知道凯拉可能已经遇害并且和铁匠有关，
+            你知道Kara可能已经遇害并且和铁匠有关，
             但当你回到河边时血腥气味已经接近消失了，
             你无法找到其他线索了，
             任务失败,你空手回到了狩魔猎人城堡。
@@ -285,17 +264,18 @@ class AfterEnding(Scene):
                 你侧身拔剑，将他刺了个对穿，
                 这是他该有的下场。
                 """))
+            sleep(5)
             print(dedent("""
                 成为杀人凶手的你拿不到赏金了，
                 并且还受到了领近村庄的通缉，
-                将凯拉的遗骸草草埋葬后，
+                将Kara的遗骸草草埋葬后，
                 你双手空空逃回了狩魔猎人城堡。
                 """))
             return 'finished'
         elif choice == '否':
             print(dedent("""
                 事情该到此为止了。
-                你将凯拉的遗骸交给村长，
+                你将Kara的遗骸交给村长，
                 并带着赏金回到了狩魔猎人城堡。
                 """))
             return 'finished'
@@ -333,14 +313,14 @@ class Map(object):
 def begeining_message():
     print(dedent("""
             你是一位狩魔猎人，
-            某天你接到了溪木村村长的委托需要调查一个小女孩凯拉的失踪事件，
+            某天你接到了溪木村村长的委托需要调查一个小女孩Kara的失踪事件，
             你从村口铁匠口中了解到她最后在河边出现。
             你来到河边，运用你的猎人感应发现了以下3样线索，
             你闻到了一股微弱的血腥气息，
             在一堆被挖开的泥土旁有一块不明动物的腐肉，
             靠近村庄方向的地上有1把玩具木剑。
-            夜晚即将降临，天黑后现场可能会被在河边出没的水鬼破坏，
-            你只来的及调查其中最多两件线索。
+            夜晚即将降临，而且看起来似乎会下雨，
+            为了防止线索消失，你应该尽快开始调查。
         """))
 
 
